5be4059c93b453405bcdc888a4116908facd9728
Kutouzi <qq454212450@gmail.com>
2022-07-18 14:37:32 +0900

    update v0.7

--- CheckData.py
+++ CheckData.py
@@ -1,17 +1,43 @@
 import pandas as pd
 
-def checkHiatusID():
-    table = pd.read_csv("./charaMap/charaData.csv",
-                        converters={'charaID':str,'charaFileID':str,'favorability':str,'randomCode':str})
-    # charaIDTable = table.loc[['charaID']]
-    numList:set = set(range(1,500))
-    hiatusTable:set = set()
-    IDTable:set = set()
-    for index,row in table.iterrows():
-        a = int(row['charaID'])
-        IDTable.add(a)
-    hiatusTable =  set(numList) - set(IDTable)
-    find =467
-    for id in hiatusTable:
-        if id == find:
-            print("exiet")
+def checkHiatusID(IDString:str):
+    if IDString.__len__() == 6 and IDString[3:4] == 'E':
+        id = IDString[:3]
+        fileID = IDString[3:]
+        table = pd.read_csv("./charaMap/charaData.csv",
+                            converters={'charaID':str,'charaFileID':str,'favorability':str,'randomCode':str})
+        # charaIDTable = table.loc[['charaID']]
+        numList:set = set(range(1,500))
+        hiatusTable:set = set()
+        IDTable:set = set()
+        for index,row in table.iterrows():
+            a = int(row['charaID'])
+            IDTable.add(a)
+        hiatusTable =  set(numList) - set(IDTable)
+        find = int(id)
+        for id in hiatusTable:
+            if id == find:
+                print("find")
+                return True
+    return False
+
+def insertID(IDString:str):
+    if IDString.__len__() == 6:
+        id = IDString[:3]
+        fileID = IDString[3:]
+        table = pd.read_csv("./charaMap/charaData.csv",
+                            converters={'charaID':str,'charaFileID':str,'favorability':str,'randomCode':str})
+        IDTable:set = set()
+        for index,row in table.iterrows():
+            a = int(row['charaID'])
+            IDTable.add(a)
+        flag = True
+        for i in IDTable:
+            if i == int(id):
+                flag = False
+                break
+        if flag == True:
+            table.loc[table.__len__()+2] = [id,fileID,'','']
+            resultTable = table.sort_values(by='charaID',ascending=True)
+            resultTable.to_csv("./charaMap/charaData.csv",index=False)
+            print("update table")
--- Test.py
+++ Test.py
@@ -1,4 +1,10 @@
 import CheckData
 
 if __name__ == '__main__':
-    CheckData.checkHiatusID()
+    step = 0
+    IDString = "450EL7"
+    if step == 0:
+        if CheckData.checkHiatusID(IDString):
+            step = 1
+    if step == 1:
+        CheckData.insertID(IDString)
